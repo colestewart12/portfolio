dd1b6b97ecab13a08441d608244c1dfde877b740
Cole Stewart <colesoccerstewy@gmail.com>
2024-09-11 10:59:33 -0700

    pages working

---
+++ app/projects/page.tsx
@@ -0,0 +1,8 @@
+export default function Projects() {
+  return (
+    <div>
+      <h1>Projects</h1>
+      <p>Here are some of the projects I have worked on.</p>
+    </div>
+  );
+}
--- app/resume/page.tsx
+++ app/resume/page.tsx
@@ -1,9 +1,9 @@
 export default function Resume(){
     return (
         <iframe
-            src="resume.pdf"
+            src="/resume.pdf"
             width="100%"
-            height="600px"
+            height="750px"
             title="PDF Viewer"
         />
     );
